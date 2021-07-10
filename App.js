60dc02bda4f6e2b6b6c8cfd479da239e7395c056
xanoosh <xanoosh@gmail.com>
2021-07-10 16:39:37 +0200

    setting up mmkv storage framework, getting data on app load with useEffect

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, Button, View } from 'react-native';
-
+import { MMKV } from 'react-native-mmkv';
 //style & components
 // import styles from './styles.js';
 import Resource from './Resource.js';
@@ -10,6 +10,19 @@ export default function App() {
   const [money, setMoney] = useState(0);
   const [steel, setSteel] = useState(0);
   const [titanium, setTitanium] = useState(0);
+  useEffect(() => {
+    //get stored data on app load
+    if (MMKV.getNumber('money')) setMoney(MMKV.getNumber('money'));
+    if (MMKV.getNumber('steel')) setSteel(MMKV.getNumber('steel'));
+    if (MMKV.getNumber('titanium')) setSteel(MMKV.getNumber('titanium'));
+  }, []);
+  useEffect(() => {
+    //get stored data on app load
+    if (MMKV.getNumber('money')) setMoney(MMKV.getNumber('money'));
+    if (MMKV.getNumber('steel')) setSteel(MMKV.getNumber('steel'));
+    if (MMKV.getNumber('titanium')) setSteel(MMKV.getNumber('titanium'));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Resource name="Money" value={money} setValue={setMoney} />
