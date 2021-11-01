27b3d8142915e388cdf9630c6db76f452ff85ba4
xanoosh <xanoosh@gmail.com>
2021-11-01 09:30:17 +0100

    switch added

--- modules/SettingsModal.js
+++ modules/SettingsModal.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Modal, Pressable, Text, View, Image } from 'react-native';
+import {
+  Modal,
+  Pressable,
+  Text,
+  View,
+  Image,
+  Switch,
+  Alert,
+} from 'react-native';
 import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake';
 import settingsImg from '../assets/settings-wheel.png';
 import { settingsModalStyles, resourceModalStyles } from '../style/styles.js';
@@ -51,17 +59,15 @@ const SettingsModal = ({
           </View>
           <View style={settingsModalStyles.settingsView}>
             <Text style={settingsModalStyles.btnHeading}>Screen always on</Text>
-            <Pressable
-              style={settingsModalStyles.btn}
-              onPress={() => {
+            <Switch
+              // trackColor={{ false: '#767577', true: '#81b0ff' }}
+              // thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+              onValueChange={() => {
                 keepAwake ? deactivateKeepAwake() : activateKeepAwake();
                 setKeepAwake(!keepAwake);
               }}
-            >
-              <Text style={settingsModalStyles.btnTxt}>
-                {keepAwake ? 'Turn off' : 'Turn on'}
-              </Text>
-            </Pressable>
+              value={keepAwake}
+            />
           </View>
         </View>
       </Modal>
